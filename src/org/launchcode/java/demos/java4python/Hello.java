5168e6ccbb8c0bd9331bdc87d0e332e3d1ed5031
swish <swisher66@gmail.com>
2019-07-17 15:57:39 -0500

    addint the Alice and RectangleArea classes to the java-exercises module.

--- src/org/launchcode/java/demos/java4python/Hello.java
+++ src/org/launchcode/java/demos/java4python/Hello.java
@@ -1,10 +1,16 @@
 package org.launchcode.java.demos.java4python;
 
+import java.util.Scanner;
 /**
  * From "Java for Python Programmers"
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+
+        Scanner in = new Scanner(System.in);
+        String userName;
+        System.out.println("What's your name?");
+        userName = in.nextLine();
+        System.out.println("Hello, " + userName + "!");
     }
 }
